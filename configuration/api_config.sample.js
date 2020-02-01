1982d84f8eeac5e87f819855900b93e76719cc21
Ben Potter <me@bpmct.net>
2020-02-01 16:15:08 -0500

    gitignored the config files that may have sensative info

---
+++ configuration/api_config.sample.js
@@ -0,0 +1,13 @@
+//Rename to api_config.js
+
+const builtonConfig = {
+  apiKey: ""
+};
+
+const firebaseConfig = {
+  apiKey: "",
+  authDomain: "",
+  databaseURL: ""
+};
+
+export { builtonConfig, firebaseConfig };
